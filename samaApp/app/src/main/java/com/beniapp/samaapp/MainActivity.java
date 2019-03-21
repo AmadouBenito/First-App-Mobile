8a9361d823eb1cf77cb58d7b262ae4661847b0df
AmadouBenito <beni.diop.95@gmail.com>
2019-03-21 13:34:20 +0000

    This is my first Andoid App

---
+++ samaApp/app/src/main/java/com/beniapp/samaapp/MainActivity.java
@@ -0,0 +1,85 @@
+package com.beniapp.samaapp;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.support.design.widget.FloatingActionButton;
+import android.support.design.widget.Snackbar;
+import android.support.v7.app.AppCompatActivity;
+import android.support.v7.widget.ButtonBarLayout;
+import android.support.v7.widget.Toolbar;
+import android.util.Log;
+import android.view.View;
+import android.view.Menu;
+import android.view.MenuItem;
+import android.widget.Button;
+import android.widget.TabHost;
+import android.widget.Toast;
+
+import java.time.Instant;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
+        setSupportActionBar(toolbar);
+        Button btn = findViewById(R.id.SamaButton);
+        // une classe anonyme
+        btn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Toast t = Toast.makeText(getApplicationContext() ,"Beuss nako" , Toast.LENGTH_LONG);
+                t.show();
+                Log.i("MAINTENANCE", "Il semble avoir erreur");
+                Intent it = new Intent(getApplicationContext(),Main2Activity.class);
+                startActivity(it);
+            }
+        });
+        
+
+
+
+        FloatingActionButton fab = (FloatingActionButton) findViewById(R.id.fab);
+        fab.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Snackbar.make(view, "Replace with your own action", Snackbar.LENGTH_LONG)
+                        .setAction("Action", null).show();
+            }
+        });
+    }
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        // Inflate the menu; this adds items to the action bar if it is present.
+        getMenuInflater().inflate(R.menu.menu_main, menu);
+        return true;
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        // Handle action bar item clicks here. The action bar will
+        // automatically handle clicks on the Home/Up button, so long
+        // as you specify a parent activity in AndroidManifest.xml.
+        int id = item.getItemId();
+
+        //noinspection SimplifiableIfStatement
+        if (id == R.id.action_settings) {
+            Log.i("MAINACTIVITY", "onOptionsItemSelected: Le SETTING");
+            return true;
+        }
+        if (id == R.id.action_help) {
+            Log.i("MAINACTIVITY", "onOptionsItemSelected: Le HELP");
+            return true;
+        }
+        if (id == R.id.action_about) {
+            Log.i("MAINACTIVITY", "onOptionsItemSelected: Le ABOUT");
+
+            return true;
+        }
+
+        return super.onOptionsItemSelected(item);
+    }
+}
